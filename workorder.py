c0a356ad3ee6dcd415a3c77e797c86af23253cd0
theautomation <github@theautomation>
2021-11-21 20:46:38 +0100

    added logging

--- workorder.py
+++ workorder.py
@@ -6,16 +6,24 @@ import json
 import os
 import sys
 import time
-import datetime
+import logging
+from datetime import date
+from datetime import timedelta
+
+## logging
+logging.basicConfig(format='%(levelname)s %(asctime)s %(message)s', filename='workorder.log', level=logging.DEBUG)
+
+logging.info("script started")
 
 ## variables
 secrets_json_filename = (".secrets.json")
 
-## datetime
-# now = datetime.datetime()
-# date_string = now.strftime("%Y")
 
-# print(date_string)
+## datetime
+today = date.today().strftime("%b " "%d")
+yesterday = (date.today() - timedelta(days=1)).strftime("%b " "%d")
+
+
 
 # set path variables
 dirname = os.path.dirname(__file__)
@@ -29,33 +37,34 @@ try:
 except FileNotFoundError:
   sys.exit(secrets_json_filename , "not exists in", dirname, "directory")
 
-# set username and password variables
+## take objects from secrets.json as variables
 username = objects_secrets_json["username"]
 password = objects_secrets_json["password"]
-target_url = objects_secrets_json["target_url"]
+url = objects_secrets_json["url"]
 
-# check if default values in secrets.json is changed
+## check if default values in secrets.json is changed
 if username == "username" or username == "":
   sys.exit("please enter a valid username in", secrets_json_filename)
 if password == "password" or password == "":
   sys.exit("please enter a valid password in", secrets_json_filename)
+if url == "url" or url == "":
+  sys.exit("please enter a valid password in", secrets_json_filename)
 
 ## start selenium
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 options = Options()
 options.headless = False
 driver = webdriver.Chrome(options=options)
-
 delay = 3
 
 ## open webpage
-driver.get(target_url)
+driver.get(url)
 
 ## enter username
 username_field = driver.find_element(By.TAG_NAME, 'input')
@@ -75,8 +84,10 @@ login_button.click()
 
 ## workorder
 try:
-    order = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(),'Nov 21')]")))
-    print(order.text, " text is located")
+    order = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(),'" + today + "')]")))
+    print("workorder is found with text", '"' + (order.text) + '"')
     order.click()
-except TimeoutException:
-    print("No workorder present with today's date")
+except TimeoutException: 
+    print("no workorder is found with text", '"' + (order.text) + '"')
+
+logging.info("script finished")
