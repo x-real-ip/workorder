3830d6d37385ba7084731b1cabe3d2626e931d2a
theautomation <github@theautomation>
2021-12-05 20:35:36 +0100

    extend api in progress

--- api.py
+++ api.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Workday(BaseModel):
+    date: str
+    start_time: str
+    end_time: str
+    description: str = None
+
 
 app = FastAPI()
 
 
-@app.get("/clock")
-def index():
-    return "Hey"
+@app.post("/workday/")
+async def workday_entry(workday: Workday):
+    return workday
