ed9002557e6262c369323ff6c502d491b2e4038f
theautomation <github@theautomation.nl>
2021-12-12 15:23:08 +0100

    test

--- app/worker/test.py
+++ app/worker/test.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python
 
-# import sqlite3
-
-
-# con = sqlite3.connect("/app/db/database.db")
-# cur = con.cursor()
-# cur.execute(
-#     "select * from workdays WHERE date=? ORDER BY id DESC LIMIT 1", ("2021-12-08",))
-# result = (cur.fetchall())
-# con.close()
-# print(result)
-
 print("test cron python")
