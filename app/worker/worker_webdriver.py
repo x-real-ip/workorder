cf7e4c4ebbb930d8a4b57ed582ffde6866102c9e
theautomation <github@theautomation.nl>
2021-12-13 09:51:14 +0100

    added chromedriver path

--- app/worker/worker_webdriver.py
+++ app/worker/worker_webdriver.py
@@ -18,7 +18,8 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
-driver = webdriver.Chrome(chrome_options=chrome_options)
+driver = webdriver.Chrome(
+    executable_path="/app/worker/chromedriver", chrome_options=chrome_options)
 
 
 def open_webpage(url):
