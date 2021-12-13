f7b36be51c7efd91fb84a478be4954e50a66b325
theautomation <github@theautomation.nl>
2021-12-13 13:34:25 +0100

    test

--- app/worker/test.py
+++ app/worker/test.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python
 
-print("test cron python")
+import os
+
+# Check os env variables
+if "WEB_URL" in os.environ:
+    web_url = os.environ.get("WEB_URL")
+    print(web_url)
+else:
+    print("no env")
