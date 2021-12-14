46dd28ba2beabf19336c4a1cc0c1a5514f27ee45
theautomation <github@theautomation.nl>
2021-12-14 13:18:55 +0100

    echo text in entry

--- app/worker/entry.sh
+++ app/worker/entry.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+# #!/bin/bash
+set -e
 
 # Print env to globally environment for cron
 printenv | grep -v "no_proxy" >> /etc/environment
 
 # Start cron
 cron -f
+
+echo "The following crontab is set:"
+
+cat /app/worker/crontab
