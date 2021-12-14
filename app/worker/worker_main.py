e5e3d64f269d4d5fa38f8bfc33dfc994a9aa71ac
theautomation <github@theautomation.nl>
2021-12-14 18:58:13 +0100

    fix logging.ini path

--- app/worker/worker_main.py
+++ app/worker/worker_main.py
@@ -14,7 +14,7 @@ from helper import logging
 import logging
 from logging.config import fileConfig
 
-fileConfig('../logging.ini')
+fileConfig("/app/logging.ini")
 logger = logging.getLogger(__name__)
 
 logger.info("started script")
