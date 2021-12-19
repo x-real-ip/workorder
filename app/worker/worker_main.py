efecff89402bfe49769114992ab323f70bbf8881
theautomation <github@theautomation.nl>
2021-12-19 13:09:19 +0100

    all in one sript

--- app/worker/worker_main.py
+++ app/worker/worker_main.py
@@ -2,31 +2,91 @@
 # script writtin by Coen Stam
 # https://github.com/theautomation
 # github@theautomation.nl
-# version 1.1.2
 
-import worker_webdriver
-import datetime
+import re
 import os
 import sys
-
-import worker_timecalc
-
-from worker_query import query
-
+import time
+import sqlite3
+import datetime
 import logging
 import logging.config
 
-# Path must be absolute because cron runs the script
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+
+
+# Log.ini Path must be absolute for cron
 logging.config.fileConfig('/app/worker/logging.ini',
                           disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
 
+def convert_date(date_input):
+    """Return date as month three letter string and day as non-zero digit. e.g. Jan 1"""
+    date_output = datetime.datetime.strptime(
+        date_input, "%Y-%m-%d").strftime("%b " "%-d")
+    return date_output
+
+
+def split_time(time):
+    """Return time 'HH', 'MM' as list, a non digit character will be used as seperator"""
+    time = re.split("\D", time)
+    hours = int(time[0])
+    minute = int(time[1])
+    return [hours, minute]
+
+
+def time_round_down(hour, minute):
+    """Return time 'HH', 'MM' with minutes rounded down to a quarter"""
+    if minute <= 15:
+        minute = 00
+    elif minute <= 30 >= 15:
+        minute = 15
+    elif minute <= 45 >= 30:
+        minute = 30
+    else:
+        minute = 45
+    return (f"{hour:02d}"), (f"{minute:02d}")
+
+
+def time_round_up(hour, minute):
+    """Return time 'HH', 'MM' with minutes rounded up to a quarter"""
+    if minute <= 15 >= 00:
+        minute = 15
+    elif minute <= 30 >= 15:
+        minute = 30
+    elif minute <= 45 >= 30:
+        minute = 45
+    else:
+        minute = 00
+        hour += 1
+        if hour == 24:
+            hour = 00
+    return (f"{hour:02d}"), (f"{minute:02d}")
+
+
+def query(database, date):
+    """Return values from database based on date"""
+    con = sqlite3.connect(database)
+    cur = con.cursor()
+    cur.execute(
+        "select * from workdays WHERE date=? ORDER BY id DESC LIMIT 1", (date,))
+    result = (cur.fetchone())
+    con.close()
+    return result
+
+
 def main():
 
-    logger.info("script started")
-
-    # env variables
+    # Check env variables
     if 'WEB_URL' in os.environ:
         web_url = os.environ.get('WEB_URL')
         logger.debug(f"using url: {web_url}")
@@ -62,31 +122,26 @@ def main():
             "No password os env find please check prd-workorder-app.env file")
         sys.exit()
 
-    # Get yesterday date as string
-    today = datetime.date.today()
-    yesterday = str(today - datetime.timedelta(days=1))
-    # Use date converter e.g. "2021-01-01 -> "1 Jan"
-    converted_date = worker_timecalc.convert_date(yesterday)
-
     # Query yesterday's time data from database
     try:
+        yesterday = str(datetime.date.today() - datetime.timedelta(days=1))
         db_query_result = query('/app/db/database.db', yesterday)
         start_time = db_query_result[2]
         end_time = db_query_result[3]
         logger.info(
-            f"found worktime in database on day {yesterday} starttime: {start_time}\ endtime: {end_time}")
+            f"found worktime in database on day {yesterday} starttime: {start_time} endtime: {end_time}")
     except TypeError:
         logger.info(
-            f"can't find any values ​​in the database on day {yesterday}, so there's nothing to do.")
+            f"can't find any values in the database on day {yesterday}, so there's nothing to do.")
         logger.info("exit worker")
         sys.exit()
 
     try:
         # Round down and round up time
-        final_start_time = worker_timecalc.time_round_down(
-            *worker_timecalc.split_time(start_time))
-        final_end_time = worker_timecalc.time_round_up(
-            *worker_timecalc.split_time(end_time))
+        final_start_time = time_round_down(
+            *split_time(start_time))
+        final_end_time = time_round_up(
+            *split_time(end_time))
         logger.info(
             f"worktime for workorder: {yesterday} is rounded up and down:")
         logger.info(
@@ -94,21 +149,98 @@ def main():
     except Exception as msg:
         logger.error(msg)
 
-    worker_webdriver.open_webpage(web_url)
+    # Setup webdriver
+    install = Service(ChromeDriverManager().install())
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=720,930")
+    driver = webdriver.Chrome(service=install, options=options)
 
-    worker_webdriver.login_webpage(web_username, web_password)
+    # Open webpage
+    try:
+        driver.get(web_url)
+        logger.debug(f"openend: {web_url}")
+    except Exception:
+        logger.debug(f"can't open: {web_url}")
 
-    worker_webdriver.open_workorder(converted_date)
+    # Login
+    driver.find_element(By.TAG_NAME, 'input').send_keys(web_username)
+    driver.find_element(By.TAG_NAME, 'button').click()
+    driver.find_element(By.TAG_NAME, 'input').send_keys(web_password)
+    driver.find_element(
+        By.XPATH, '//*[@id="app"]/div/div[2]/div[1]/div[2]/button[2]').click()
 
-    worker_webdriver.fill_in_form(*final_start_time, *final_end_time)
+    # Convert date e.g. "2021-01-01 -> "Jan 1"
+    converted_date = convert_date(yesterday)
 
-    worker_webdriver.goto_send()
+    # Search workorder based on converted date string and text in workorder
+    try:
+        order = WebDriverWait(driver, 3).until(EC.presence_of_element_located(
+            (By.XPATH, "//div/p[contains(text(),'{}') and preceding-sibling::h6[contains(text(),'dienst') or contains(text(),'Weekenddienst') or contains(text(),'Operator') or contains(text(),'motorkap')]]".format(converted_date))))
+        order.click()
+    except Exception:
+        logger.info(f"no workorder was found, closing webdriver")
+        driver.quit()
+        sys.exit()
 
-    worker_webdriver.screenshot(yesterday)
+    # Enter time in workorder
+    start_hours = WebDriverWait(driver, 5).until(EC.presence_of_element_located(
+        (By.XPATH, '//div[1]/span/input[1][@class="time-input hours"]')))
+    start_hours.send_keys(Keys.BACKSPACE)
+    start_hours.send_keys(final_start_time[0])
 
-    worker_webdriver.send_workorder()
+    start_minutes = driver.find_element(
+        By.XPATH, '//input[2][@class="time-input minutes"]')
+    start_minutes.send_keys(final_start_time[1])
 
-    worker_webdriver.quit()
+    logger.info(
+        f"starttime: {final_start_time[0]}:{final_start_time[1]} has filled in")
+
+    end_hours = driver.find_element(
+        By.XPATH, "//label[contains(text(),'End')]/following-sibling::span/input[@class='time-input hours']")
+    end_hours.send_keys(Keys.BACKSPACE)
+    end_hours.send_keys(final_end_time[0])
+
+    end_minutes = driver.find_element(
+        By.XPATH, "//label[contains(text(),'End')]/following-sibling::span/input[@class='time-input minutes']")
+    end_minutes.send_keys(final_end_time[1])
+
+    logger.info(
+        f"endtime: {final_end_time[0]}:{final_end_time[1]} has filled in")
+
+    # Go to send button
+    actions = ActionChains(driver)
+    actions.send_keys(Keys.TAB * 7)
+    actions.perform()
+    time.sleep(5)
+
+    # Screenshot
+    screenshot = os.environ.get('SAVE_IMAGE', "false")
+    if screenshot == "true":
+        path = (f"/app/log/workorder_{yesterday}.png")
+        try:
+            driver.save_screenshot(path)
+            logger.info(
+                f"image of entered workorder saved. location: {path}")
+        except Exception:
+            logger.error("can't save image of workorder")
+
+    # Send
+    try:
+        actions = ActionChains(driver)
+        actions.send_keys(Keys.ENTER)
+        # actions.perform()
+    except Exception:
+        logger.error("workorder was not being send")
+        sys.exit()
+    else:
+        logger.info("workorder send successfully")
+
+    # Quit webdriver
+    driver.quit()
+    logger.info("selenium webdrive finished")
 
 
 if __name__ == '__main__':
