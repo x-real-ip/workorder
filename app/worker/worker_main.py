a439508a1a4c2543e6e8d6591e484503f4a79206
theautomation <github@theautomation.nl>
2022-01-19 10:06:34 +0100

    add windows size chrome option

--- app/worker/worker_main.py
+++ app/worker/worker_main.py
@@ -139,6 +139,7 @@ def main():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=720,930")
     chrome_prefs = {}
     chrome_options.experimental_options["prefs"] = chrome_prefs
     chrome_prefs["profile.default_content_settings"] = {"images": 2}
